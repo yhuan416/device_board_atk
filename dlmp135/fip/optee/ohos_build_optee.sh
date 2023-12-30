e0c798e6971a0f607d2114530f9750da50974c6f
yhuan416 <yhuan416@foxmail.com>
2023-12-30 20:20:06 +0800

    更新脚本

--- dlmp135/fip/optee/ohos_build_optee.sh
+++ dlmp135/fip/optee/ohos_build_optee.sh
@@ -2,25 +2,19 @@
 
 echo "Build OPTEE for dlmp135"
 
-ROOT_BUILD_DIR=${1}
-ROOT_DIR=${ROOT_BUILD_DIR}/../..
+OPTEE_SRC_PATH=${1}
+OPTEE_BLD_PATH=${2}
+OPTEE_DEPLOYDIR=${3}
 
-echo "ROOT_BUILD_DIR=${ROOT_BUILD_DIR}"
-echo "ROOT_DIR=${ROOT_DIR}"
-
-OPTEE_SRC_PATH=${ROOT_DIR}/device/board/atk/dlmp135/fip/optee
-
-OPTEE_BLD_PATH=${ROOT_BUILD_DIR}/fip/optee/build
-OPTEE_DEPLOYDIR=${ROOT_BUILD_DIR}/fip/deploy
+echo "OPTEE_SRC_PATH=${OPTEE_SRC_PATH}"
+echo "OPTEE_BLD_PATH=${OPTEE_BLD_PATH}"
+echo "OPTEE_DEPLOYDIR=${OPTEE_DEPLOYDIR}"
 
 # rm build dir
 rm -rf ${OPTEE_BLD_PATH}
 
 # enter optee src dir
-cd ${OPTEE_SRC_PATH}/optee-os-stm32mp-3.16.0-stm32mp1-r1
-
-echo ${PATH}
+cd ${OPTEE_SRC_PATH}
 
 # build 使用环境变量自带的python3
 PATH=/usr/bin/:${PATH} BLD_PATH=${OPTEE_BLD_PATH} DEPLOYDIR=${OPTEE_DEPLOYDIR} make -f ../Makefile.sdk all
-
--- dlmp135/fip/uboot/ohos_build_uboot.sh
+++ dlmp135/fip/uboot/ohos_build_uboot.sh
@@ -2,28 +2,16 @@
 
 echo "Build U-BOOT for dlmp135"
 
-ROOT_BUILD_DIR=${1}
-ROOT_DIR=${ROOT_BUILD_DIR}/../..
-
-echo "ROOT_BUILD_DIR=${ROOT_BUILD_DIR}"
-echo "ROOT_DIR=${ROOT_DIR}"
-
-UBOOT_SRC_PATH=${ROOT_DIR}/device/board/atk/dlmp135/fip/uboot
-
-UBOOT_BLD_PATH=${ROOT_BUILD_DIR}/fip/uboot/build
-UBOOT_DEPLOYDIR=${ROOT_BUILD_DIR}/fip/deploy
+UBOOT_SRC_PATH=${1}
+UBOOT_BLD_PATH=${2}
+UBOOT_DEPLOYDIR=${3}
 
 # rm build dir
 rm -rf ${UBOOT_BLD_PATH}
 
 # enter uboot src dir
-cd ${UBOOT_SRC_PATH}/u-boot-stm32mp-v2021.10-stm32mp1-r1
-
-echo ${PATH}
+cd ${UBOOT_SRC_PATH}
 
 # build 使用环境变量自带的python3
-PATH=/usr/bin/:${PATH} \
-BLD_PATH=${UBOOT_BLD_PATH} \
-DEPLOYDIR=${UBOOT_DEPLOYDIR} \
+PATH=/usr/bin/:${PATH} BLD_PATH=${UBOOT_BLD_PATH} DEPLOYDIR=${UBOOT_DEPLOYDIR} \
 make CROSS_COMPILE=arm-none-linux-gnueabihf- -f ../Makefile.sdk all
-
